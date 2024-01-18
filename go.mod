0f28225dbf0aa25da46349618553190b1d0a8b84
sty-holdings <111923061+sty-holdings@users.noreply.github.com>
2024-01-17 20:23:30 -0800

    Refactoring several areas with testing
    CoreProgramInfo
    CoreConfiguration
    CoreOS
    main.go - add support for cli, start up validation, display of config skeleton

---
+++ go.mod
@@ -0,0 +1,62 @@
+module GriesPikeThomp
+
+go 1.21
+
+require (
+	cloud.google.com/go/firestore v1.14.0
+	cloud.google.com/go/storage v1.36.0
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/aws/aws-sdk-go v1.49.23
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/google/uuid v1.5.0
+	github.com/integrii/flaggy v1.5.2
+	github.com/jackc/pgx/v5 v5.5.2
+	github.com/nats-io/nats.go v1.32.0
+	github.com/plaid/plaid-go/v9 v9.0.0
+	github.com/sendgrid/rest v2.6.9+incompatible
+	github.com/sendgrid/sendgrid-go v3.14.0+incompatible
+	github.com/sty-holdings/resuable-const-vars v2024.4.3+incompatible
+	golang.org/x/text v0.14.0
+	google.golang.org/api v0.156.0
+)
+
+require (
+	cloud.google.com/go v0.111.0 // indirect
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.5 // indirect
+	cloud.google.com/go/longrunning v0.5.4 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.46.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
---
+++ servers/nats-connect/main.go
@@ -0,0 +1,113 @@
+/*
+This is the STY-Holdings NATS-Connect service
+
+NOTES:
+
+	None
+
+COPYRIGHT & WARRANTY:
+
+	Copyright (c) 2022 STY-Holdings, inc
+	All rights reserved.
+
+	This software is the confidential and proprietary information of STY-Holdings, Inc..
+	Use is subject to license terms.
+
+	Unauthorized copying of this file, via any medium is strictly prohibited.
+
+	Proprietary and confidential
+
+	Written by Scott Yacko / syacko
+	STY-Holdings, Inc.
+	support@sty-holdings.com
+	<Replace with WEBSITE_NAME>
+
+	12-2023
+
+	USA
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	cc "GriesPikeThomp/shared-services/src/coreConfiguration"
+	cpi "GriesPikeThomp/shared-services/src/coreProgramInfo"
+	"github.com/integrii/flaggy"
+	rcv "github.com/sty-holdings/resuable-const-vars/src"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+// Add types to the request_reply_types.go or the data_structure_types.go file
+
+var (
+	// Add Variables here for the file (Remember, they are global)
+	// Start up values for a service
+	serverName         = ""
+	generateConfigFile bool
+	configFileFQN      string
+	version            = "9999.9999.9999"
+)
+
+func init() {
+
+	appDescription := "The " + cases.Title(language.English).String(serverName) + " bridges the gap between NATS users and the wider world, enabling effortless integration" +
+		" with third-party platforms and services.\n" +
+		"\nVersion: \n" +
+		rcv.SPACES_FOUR + "- " + version + "\n" +
+		"\nConstraints: \n" +
+		rcv.SPACES_FOUR + "- When using -c you must pass the fully qualified configuration file name.\n" +
+		rcv.SPACES_FOUR + "- There is no console available at this time and all log messages are output to Log_Directory specified in the config file.\n" +
+		"\nNotes:\n" +
+		rcv.SPACES_FOUR + "None\n" +
+		"\nFor more info, see link below:\n"
+
+	// Set your program's name and description.  These appear in help output.
+	flaggy.SetName("\n" + serverName) // "\n" is added to the start of the name to make the output easier to read.
+	flaggy.SetDescription(appDescription)
+
+	// You can disable various things by changing bool on the default parser
+	// (or your own parser if you have created one).
+	flaggy.DefaultParser.ShowHelpOnUnexpected = true
+
+	// You can set a help prepend or append on the default parser.
+	flaggy.DefaultParser.AdditionalHelpPrepend = "https://github.com/styh-dev/albert"
+
+	// Add a flag to the main program (this will be available in all subcommands as well).
+	flaggy.String(&configFileFQN, "c", "config", "Provides the setup information needed by and is required to start the server.")
+	flaggy.Bool(&generateConfigFile, "g", "genconfig", "This will output a skeleton configuration file.\n\t\t\tThis will cause the -c option to be ignored.")
+
+	// Set the version and parse all inputs into variables.
+	flaggy.SetVersion(version)
+	flaggy.Parse()
+}
+
+func main() {
+
+	if serverName == rcv.VAL_EMPTY {
+		cpi.PrintError(cpi.ErrMissingServerName, fmt.Sprintf("%v %v", rcv.TXT_SERVER_NAME, serverName), rcv.MODE_OUTPUT_DISPLAY)
+		os.Exit(1)
+	}
+
+	if generateConfigFile {
+		cc.GenerateConfigFileSkeleton(serverName, cc.DEFAULT_SKELETON_CONFIG_DIRECTORY, cc.DEFAULT_SKELETON_CONFIG_FILENAME_NO_SUFFIX)
+		os.Exit(0)
+	}
+
+	// Has the config file location and name been provided, if not, return help.
+	if configFileFQN == "" {
+		flaggy.ShowHelpAndExit("")
+	}
+
+	log.Printf("Shutting down %v server.\n", serverName)
+	os.Exit(0)
+}
